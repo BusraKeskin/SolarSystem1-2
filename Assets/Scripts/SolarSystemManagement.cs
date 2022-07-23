127c598c27fc0f81e5a86f4be8e05368bb785008
Büşra Keskin <wwbusrakeskin@gmail.com>
2022-07-23 12:23:39 +0300

    Debug.Log Added

--- Assets/Scripts/SolarSystemManagement.cs
+++ Assets/Scripts/SolarSystemManagement.cs
@@ -6,14 +6,44 @@ public class SolarSystemManagement : MonoBehaviour
 {
 
     public Transform target;
-    public int speed;
-
+    public float speed;
+    private bool isTriggered = false;
 
     void Update()
     {
+        TurnAround();
+    }
+
+    private void TurnAround()
+    {
         transform.RotateAround(target.transform.position, target.transform.up, speed * Time.deltaTime);
     }
-   
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isTriggered)
+        {
+            if (
+                other.gameObject.tag == "YearCounter"
+                && gameObject.tag == "Celestial" && target.tag == "Sun"
+            )
+            {
+                Debug.Log(gameObject.name + " completed one year!");
+                isTriggered = true;
+
+            }
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "YearCounter" && gameObject.tag == "Celestial" && target.tag == "Sun")
+        {
+            isTriggered = false;
+        }
+    }
+    
 }
 
 
